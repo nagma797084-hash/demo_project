30fd9013d43375d9c3090f3cc9a07b225fa563dc
nagmakhatoon0011 <nagmaedugaon786@gmail.com>
2025-10-17 15:35:23 +0530

    2 changed in this app.

---
+++ lib/signup_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
+class SignupPage extends StatefulWidget {
+  const SignupPage({super.key});
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Column(
+      children: [
+        Text("hello guys")
+      ],
+    ));
+  }
+}
