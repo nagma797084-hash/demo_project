bad195d5e4a294ae0ae4effac9a1a6328c250908
nagmakhatoon0011 <nagmaedugaon786@gmail.com>
2025-10-19 12:39:34 +0530

    1 changed in this app.

---
+++ lib/demo+page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class demp extends StatefulWidget {
+  const demp({super.key});
+
+  @override
+  State<demp> createState() => _dempState();
+}
+
+class _dempState extends State<demp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
